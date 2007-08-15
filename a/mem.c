5848782b8e6016f636fba930bc2dc38368917cfd
schallee <schallee>
2007-08-15 03:21:58 +0000

    move of xmalloc funcs and central mem location

---
+++ a/mem.c
@@ -0,0 +1,46 @@
+#include <a/mem.h>
+#include <a/die.h>
+
+/* FIXME, autoconf */
+#include <stdlib.h>
+#include <string.h>
+
+void a_xfree(void *ptr)
+{
+	if(ptr)
+		free(ptr);
+}
+
+void *a_xmalloc(size_t size)
+{
+	void *out;
+
+	if(!(out = malloc(size)))
+		a_die(perror, "could not malloc(%u)", (unsigned int)size);
+	return out;
+}
+
+void *a_xrealloc(void *ptr, size_t size)
+{
+	void *out;
+
+	if(!size)
+	{
+		a_xfree(ptr);
+		return NULL;
+	}
+	if(!ptr)
+		return a_xmalloc(size);
+	if(!(out = realloc(ptr, size)))
+		a_die(perror, "could not realloc(ptr, %u)", (unsigned int)size);
+	return out;
+}
+
+char *a_xstrdup(const char* str)
+{
+	char *out;
+
+	if(!(out = strdup(str)))
+	a_die(perror, "could not strdup(\"%s\")", str);
+	return out;
+}
