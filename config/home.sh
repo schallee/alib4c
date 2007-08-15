bf07266f8660d4e9c9480f853eda336644520f0d
schallee <schallee>
2007-08-15 12:31:10 +0000

    usefull scripts

---
+++ config/home.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+for dir in . ..; do
+	if [ -f "$dir/configure.ac" ]; then
+		top_dir="$dir";
+		break;
+	fi
+done
+
+exec $top_dir/config/configure_wrap --enable-mass-warnings --prefix=$HOME --bindir=$HOME/bin/$CONFIG_GUESS --libdir=$HOME/lib/$CONFIG_GUESS --includedir=$HOME/include/$CONFIG_GUESS
---
+++ config/mingw_home.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+for dir in . ..; do
+	if [ -f "$dir/configure.ac" ]; then
+		top_dir="$dir";
+		break;
+	fi
+done
+
+platform=i586-mingw32msvc
+exec $top_dir/config/configure_wrap	\
+	--enable-mass-warnings	\
+	--enable-debug	\
+	--prefix=$HOME	\
+	--host=$platform	\
+	--bindir=$HOME/bin/$platform	\
+	--libdir=$HOME/lib/$platform	\
+	--includedir=$HOME/include/$platform \
+
