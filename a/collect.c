acfa01906f9b5e6c66fac428afe0906cc983a700
schallee <schallee>
2011-06-05 23:27:20 +0000

    Equals funcs for intptr, uintptr and const char *.

--- a/collect.c
+++ a/collect.c
@@ -7,12 +7,27 @@ extern int a_collect_cmp_intptr(const void *a, const void *b)
 	return (((intptr_t)a) - ((intptr_t)b));
 }
 
+extern bool a_collect_eq_intptr(const void *a, const void *b)
+{
+	return a==b;
+}
+
 extern int a_collect_cmp_uintptr(const void *a, const void *b)
 {
 	return (((uintptr_t)a) - ((uintptr_t)b));
 }
 
+extern bool a_collect_eq_uintptr(const void *a, const void *b)
+{
+	return a==b;
+}
+
 extern int a_collect_cmp_str(const void *a, const void *b)
 {
 	return strcmp((const char *)a, (const char *)b);
+}
+
+extern bool a_collect_eq_str(const void *a, const void *b)
+{
+	return !strcmp((const char *)a, (const char *)b);
 }
--- a/collect.h
+++ a/collect.h
@@ -13,8 +13,16 @@ typedef uint_least8_t a_collect_cb_ret_t;
 typedef a_collect_cb_ret_t (*a_collect_cb_t)(void *item, void *arg);
 
 typedef int (*a_collect_cmp_t)(const void *a, const void *b);
+typedef bool (*a_collect_eq_t)(const void *a, const void *b);
+typedef unsigned int (*a_collect_hash_t)(const void *key);
+typedef unsigned int (*a_collect_hash_bytes_t)(const void *key, size_t len);
 
 int a_collect_cmp_intptr(const void *a, const void *b);
+extern int a_collect_cmp_intptr(const void *a, const void *b);
+extern bool a_collect_eq_intptr(const void *a, const void *b);
 extern int a_collect_cmp_uintptr(const void *a, const void *b);
+extern bool a_collect_eq_uintptr(const void *a, const void *b);
+extern int a_collect_cmp_str(const void *a, const void *b);
+extern bool a_collect_eq_str(const void *a, const void *b);
 
 #endif
