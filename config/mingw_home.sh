055226f2666e57251aeeb2e07d814805ba2c5543
schallee <schallee>
2011-06-05 23:33:47 +0000

    Disable debugging, append cmd line arguments

--- config/mingw_home.sh
+++ config/mingw_home.sh
@@ -8,13 +8,11 @@ for dir in . ..; do
 done
 
 platform=i586-mingw32msvc
-CFLAGS=-DA_DEBUG=1
 exec $top_dir/config/configure_wrap	\
 	--enable-mass-warnings	\
-	--enable-debug	\
 	--prefix=$HOME	\
 	--host=$platform	\
 	--bindir=$HOME/bin/$platform	\
 	--libdir=$HOME/lib/$platform	\
 	--includedir=$HOME/include/$platform \
-
+	"$@"
