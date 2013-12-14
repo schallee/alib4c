c1f7d8d1c243dc51a175853630e191406768b70e
Ed Schaller <schallee@darkmist.net>
2013-12-14 09:43:22 -0700

    Add notes about possible additions for msvc

--- a/cc.h
+++ a/cc.h
@@ -11,4 +11,11 @@
 #	define A_CC_NO_RETURN
 #endif
 
+/* FIXME: MSVC
+	printf - there is something in MSVC which might do this: _Printf_format_string_ see http://msdn.microsoft.com/en-us/library/ms235402%28v=VS.100%29.aspx 
+	noreturn - see tsb forensic, also there's a post c99 standard for this.
+
+	related: http://msdn.microsoft.com/en-us/library/d9x1s805.aspx
+*/
+
 #endif
